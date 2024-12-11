d0dfe9ebecae4d39aeffa88272bcd2c1e39727d1
Gerry Slade <gerry.slade@googlemail.com>
2024-12-11 16:53:16 +0100

    changes

--- fake_news_detection/api/fast.py
+++ fake_news_detection/api/fast.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#@app.get("/")
+#def root():
+    #return {
+   # 'greeting': 'Hello'
+#}
+
+
 @app.get("/")
-def root():
-    return {
-    'greeting': 'Hello'
-}
+async def root():
+    return {"message": "API is live"}
+
+@app.post("/predict")
+async def predict(text):
+    model=None
+    #model.predict(process_data)
+    # Add prediction logic here
+    return {"prediction": "fake"}  # Example response
