49afb503ec51964ac3acd46d0d35f8f77e85c68c
Zouhair Lahrech <lahrech.it@gmail.com>
2024-12-16 11:42:00 +0100

    code refactoring

--- fake_news_detection/api/fast.py
+++ fake_news_detection/api/fast.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fake_news_detection.interface.main import pred_base_model
+from typing import List
+from fake_news_detection.ml_logic.models.model_factory import model_factory
 
 app = FastAPI()
 
-# Pydantic Modell für den Body der Anfrage
+# Pydantic model for the request body
 class TextRequest(BaseModel):
     text: str
+    models: List[str]  # List of selected models (e.g. ["mstl", "base", "rnn"])
 
 @app.get("/")
 async def root():
@@ -15,17 +17,24 @@ async def root():
 @app.post("/predict")
 async def predict(request: TextRequest):
     """
-    Receive a text input, pass it to the model for making predict,
-    and return the result and accuracy as response.
+    Receives a text and a list of selected models,
+    performs predictions, and returns the results directly as JSON.
     """
-    # Call pred_base_model to get the prediction and accuracy
+    # Dictionary to hold the results
+    response = {}
+    for model_key in request.models:
+        # Assuming model_factory.getModel(model_key) returns a model instance that has a 'predict' method
+        model = model_factory.getModel(model_key)
 
-    y_pred, accuracy = pred_base_model(request.text)
+        # Get the prediction and accuracy
+        y_pred, accuracy = model.predict(request.text)
+        print('y_pred: ' + f"{y_pred}")
+        print('accuracy: ' + f"{accuracy}")
 
-    # Create the response dictionary
-    response = {
-        "prediction": bool(y_pred),
-        "accuracy": accuracy
-    }
+        # Add the result to the response dictionary
+        response[model_key] = {
+            "prediction": bool(y_pred),
+            "accuracy": accuracy if accuracy is not None else 0  # default accuracy to 0 if None
+        }
 
     return response
