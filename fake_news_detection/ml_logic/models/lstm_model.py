dd41b314973d49fa4f5a16d3819125577d089d13
Zouhair Lahrech <lahrech.it@gmail.com>
2024-12-17 02:24:52 +0100

    rnn and lstm

--- fake_news_detection/ml_logic/models/lstm_model.py
+++ fake_news_detection/ml_logic/models/lstm_model.py
@@ -1,26 +1,22 @@
-from keras.models import Sequential
 import numpy as np
-from fake_news_detection.ml_logic.models.abstract_model import abstract_model
+import tensorflow as tf
+import pandas as pd
 
 from keras.models import Sequential
+from fake_news_detection.ml_logic.models.abstract_model import abstract_model
+from colorama import Fore, Style
+from fake_news_detection.params import *
+from fake_news_detection.ml_logic.preprocessor import basic_cleaning, preprocess_feature
+from fake_news_detection.ml_logic.data import get_processed_data, split_data
+from fake_news_detection.ml_logic.registry import save_model, save_results
 
 class lstm_model(abstract_model[Sequential]):
 
-    def __init__(self, model: Sequential):
-        super().__init__(model)
+    def __init__(self, model_type: str, model: Sequential):
+        super().__init__(model_type, model)
+        self.model_type = model_type
+        self.model = model
 
-    def train(self,
-        X_train_processed: np.ndarray,
-        y_train: np.ndarray):
-        pass
-
-    def score(self,
-            X_test_processed: np.ndarray,
-            y_test: np.ndarray):
-        pass
-
-    def predict(self, news: str):
-        return (
-            False,
-            0.88
-            )
+    def compile(self):
+        # Compile the model with Adam optimizer and binary cross-entropy loss
+        self.model.compile(optimizer=tf.keras.layers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
--- fake_news_detection/ml_logic/models/rnn_model.py
+++ fake_news_detection/ml_logic/models/rnn_model.py
@@ -1,24 +1,19 @@
-from keras.models import Sequential
 import numpy as np
+from keras.models import Sequential
 from fake_news_detection.ml_logic.models.abstract_model import abstract_model
+import tensorflow as tf
+from colorama import Fore, Style
+from fake_news_detection.params import *
+from fake_news_detection.ml_logic.registry import load_metrics
+from fake_news_detection.ml_logic.preprocessor import basic_cleaning, preprocess_feature
 
 class rnn_model(abstract_model[Sequential]):
-    def __init__(self, model: Sequential):
-        super().__init__(model)
+    def __init__(self, model_type: str, model: Sequential):
+        super().__init__(model_type, model)
+        self.model_type = model_type
+        self.model = model
 
-    def train(self,
-        X_train_processed: np.ndarray,
-        y_train: np.ndarray):
-        pass
-
-    def score(self,
-            X_test_processed: np.ndarray,
-            y_test: np.ndarray):
-        pass
-
-    pass
-    def predict(self, news: str):
-        return (
-            False,
-             0.95
-            )
+    def compile(self):
+        # Adjust the Adam optimizer parameters
+        adam_optimizer = optimizer=tf.keras.layers.Adam()(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
+        self.model.compile(optimizer=adam_optimizer, loss='binary_crossentropy', metrics=['accuracy'])
