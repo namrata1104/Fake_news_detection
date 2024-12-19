f585ea5350149f0a521d2bddb87ff8e8cbd37260
Zouhair Lahrech <lahrech.it@gmail.com>
2024-12-19 10:16:44 +0100

    refactorng

--- nltk_downloader.py
+++ nltk_downloader.py
@@ -6,5 +6,5 @@ def download_nltk_data():
     nltk.download("punkt")
     nltk.download("stopwords")
 
-if __name__ == "__main__":
-    download_nltk_data()
+#if __name__ == "__main__":
+#    download_nltk_data()
